3960ad782d28d1c74edffe900142339e9ba631b3
AnduB85 <barzan.alexandru@gmail.com>
2023-05-04 22:27:50 +0300

    Prima tema

--- Assignments/BarzanCristianAlexandru/TEMA1/Triangle.py
+++ Assignments/BarzanCristianAlexandru/TEMA1/Triangle.py
@@ -8,11 +8,10 @@ class Triangle:
     def get_perimeter(self):
         return self.latura1 + self.latura2 + self.baza
 
-    def get_semiperimeter(self):
-        return (self.latura1 + self.latura2 + self.baza) * 0.5
-    def describe(self):
-        return self.culoare
-
+    def get_aria(self):
+        sp = self.get_perimeter() / 2 # acesta e semiperimetrul triunghiului
+        return (sp * (sp - self.latura1) * (sp - self.latura2) * (sp - self.baza)) ** 0.5
 t1 = Triangle(2, 3, 4 , 'Galben')
-print(f'Eu sunt primul triunghi de culoare {t1.culoare}, am latura1 {t1.latura1}, latura2 {t1.latura2} ,baza{t1.baza}  cu perimetrul {t1.get_perimeter()} si semiperimetrul {t1.get_semiperimeter()}')
-
+print(f'Eu sunt primul triunghi de culoare {t1.culoare}, am latura1 {t1.latura1}, latura2 {t1.latura2} si baza {t1.baza}')
+print(f'Perimetrul este {t1.get_perimeter()}')
+print (f'Aria este {t1.get_aria()}')
